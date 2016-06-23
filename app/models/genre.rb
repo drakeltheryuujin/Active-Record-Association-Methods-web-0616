d7a984ddcc9225576c3791a859903afc415a0594
Kellye Greene <kellye@gust.com>
2016-06-23 17:56:19 -0400

    AR associations

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,14 +3,20 @@ class Genre < ActiveRecord::Base
   has_many :artists, through: :songs
 
   def song_count
-    # return the number of songs in a genre
+    Song.all.map do |songs|
+      songs.genre_id
+    end.size
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    Song.all.map do |songs|
+      songs.artist_id
+    end.size
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    Artist.all.map do |names|
+      names.name
+    end
   end
 end
